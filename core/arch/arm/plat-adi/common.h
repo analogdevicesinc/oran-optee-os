1b905fd6e5a93342653b1f04d4907c0c12cfab45
Eduardo Grande <eduardo.grande@analog.com>
2025-02-03 10:45:54 -0500

    Revert "TPGSWE-17386: Remove bootrom bypass related code"
    This reverts commit 93b570f0d1d0fad0818c8cd2174854c551734bfe.

--- core/arch/arm/plat-adi/common.h
+++ core/arch/arm/plat-adi/common.h
@@ -14,6 +14,7 @@ void common_main_init_gic(void);
 uint32_t plat_get_sysclk_freq(void);
 uint32_t plat_get_anti_rollback_counter(void);
 uint32_t plat_get_te_anti_rollback_counter(void);
+bool plat_get_bootrom_bypass_enabled(void);
 void plat_error_message(const char *fmt, ...);
 void plat_warn_message(const char *fmt, ...);
 void plat_runtime_error_message(const char *fmt, ...);
